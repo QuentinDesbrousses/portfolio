105e4e9569ca5789404ad031a9f01182d03d8d89
Quentin Desbrousses <quentin.desbrousses@etu.umontpellier.fr>
2023-11-06 20:40:20 +0000

    Create architecture

---
+++ portfolio/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="dark:bg-dark bg-light dark:text-light text-dark">
+      <div className="dark:bg-secondary">Hello World</div>
+      <button className="bg-d-cta">Click</button>
+    </div>
+  );
+};
+
+export default Home;

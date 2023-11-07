9a450b8514a7e5f8edbd6800fa250ddc06a1e9b1
Quentin Desbrousses <quentin.desbrousses@etu.umontpellier.fr>
2023-11-07 00:13:29 +0000

    Init theme colors

--- portfolio/tailwind.config.ts
+++ portfolio/tailwind.config.ts
@@ -1,20 +1,20 @@
-import type { Config } from 'tailwindcss'
+import type {Config}
+from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        colors: {
+            'cta': '#B950F2',
+            'secondary': '#F2F4F7',
+            'd-secondary': '#282C36',
+            'light': '#DADED3',
+            'dark': '#735B51'
+        },
+        extend: {}
     },
-  },
-  plugins: [],
+    plugins: []
 }
 export default config
